2d8872568af0567c9866d47da3640e1a060fe555
Sam Katakouzinos <sam.katakouzinos@gmail.com>
2019-10-21 08:26:54 +1100

    Apply --input parameter correctly

--- scripts/addVendor.sh
+++ scripts/addVendor.sh
@@ -9,7 +9,7 @@ sudo apt-get install brotli
 mkdir -p $currentPath/system_dump/ && cd $currentPath/system_dump/
 wget $URL
 unzip lineage-*.zip system.transfer.list system.new.dat*
-brotli --decompress system.new.dat.br --output=system.new.dat
+brotli --decompress --input system.new.dat.br --output=system.new.dat
 wget https://raw.githubusercontent.com/xpirt/sdat2img/master/sdat2img.py
 python sdat2img.py system.transfer.list system.new.dat system.img
 mkdir system && sudo mount system.img system/
